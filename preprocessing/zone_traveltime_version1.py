38074eae7a313af48fc909b6876ef13bc39be7cb
sabijn <sabijn.perdijk@gmail.com>
2022-01-24 17:03:12 +0100

    use of victors graph

--- preprocessing/zone_traveltime_version1.py
+++ preprocessing/zone_traveltime_version1.py
@@ -9,14 +9,13 @@ G = nx.read_gpickle('Graph-Manhattan.pickle')
 # Create geodataframes with the nodes and the edges
 nodes = ox.graph_to_gdfs(G, edges=False)
 edges = ox.graph_to_gdfs(G, nodes=False)
-print(nodes.index)
 
-# # create zone_traveltime
-# nodes_id = list(nodes.index)
-# csv_path = pd.read_csv('zone_path.csv', header=None)
+# create zone_traveltime
+nodes_id = list(nodes.index)
+csv_path = pd.read_csv('zone_path.csv', header=None)
 
-# for start_node_id in nodes_id:
-#     for dest_node_id in nodes_id:
-#         edge = G.get_edge_data(start_node_id, dest_node_id)
-#         travel_time = edge[0]['travel_time']
-#         csv_path[start_node_id][dest_node_id] = travel_time
+for start_node_id in nodes_id:
+    for dest_node_id in nodes_id:
+        edge = G.get_edge_data(start_node_id, dest_node_id)
+        travel_time = edge[0]['travel_time']
+        csv_path[start_node_id][dest_node_id] = travel_time
