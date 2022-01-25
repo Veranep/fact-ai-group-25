24fb0d065300a4afbcca1bc3ca5c4e8984a3514f
Vera Neplenbroek <veraneplenbroek@icloud.com>
2022-01-25 13:16:59 +0100

    kmeans for paper replication

---
+++ preprocessing/kmeans.py
@@ -0,0 +1,19 @@
+import pickle
+from sklearn.cluster import KMeans
+
+def write_kmeans():
+
+    zone_lat_long = open("../../data/paper_replication/zone_latlong.csv").read().split("\n")
+    d = {}
+    coords = []
+    for i in zone_lat_long:
+        if i!='':
+            a,b,c = i.split(",")
+            d[a] = (float(b),float(c))
+            coords.append((float(b),float(c)))
+
+    regions = KMeans(n_clusters=10).fit(coords)
+    labels = regions.labels_
+    centers = regions.cluster_centers_
+
+    pickle.dump(labels,open("../../data/paper_replication/new_labels.pkl","wb"))
