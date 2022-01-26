c6cb6feffadfcb8427132bc69eef84790f745c68
sabijn <sabijn.perdijk@gmail.com>
2022-01-26 22:35:23 +0100

    flow files for green taxi

--- preprocessing/flow_files_green.py
+++ preprocessing/flow_files_green.py
@@ -9,30 +9,70 @@ from collections import defaultdict
 from get_graph_Brooklyn import get_graph
 import time
 
-def print_txt(data, day):
-    f = open(f"out/green_flow_5000_{day}.txt", 'w')
-    sys.stdout = f  
-    print(len(data))
-    for key in data:
-        print(f'Flows:{key}-{key}')
-        for flows in data[key]:
-            print(f'{flows[0]}, {flows[1]}, {float(data[key][flows])}')
-    f.close()
-    return
-
 G = get_graph()
 nodes = ox.graph_to_gdfs(G, edges=False)
-osmid_to_nodeid = dict(zip(list(nodes.index), list(range(nodes.shape[0]))))
-formatstring = '%Y-%m-%d %H:%M:%S'
+GET_NODEID = dict(zip(list(nodes.index), list(range(nodes.shape[0]))))
+
+def filter_list(list_to_filter, list_with_indices):
+    """
+    Returns a list with all entries list_to_filter[i] for all i in list_with_indices
+    """
+    return np.array(list_to_filter)[list_with_indices]
+
+def print_txt(data, day):
+    output = f'{len(data.keys())}\n'
+    
+    for key in data:
+        output += f'Flows:{key}-{key}\n'
+        for flows in data[key]:
+            output += f'{flows[0]}, {flows[1]}, {float(data[key][flows])}\n'
+    f = open(f"out/brooklyn/test_flow_5000_{day}.txt", 'w')
+    f.write(output)
+    return
+
+def process_requests(start_lngs, start_lats, dest_lngs, dest_lats, flows_list):
+    # Retrieve pickup and destination from csv
+    start_osmids, start_dists = ox.distance.nearest_nodes(G, start_lngs, start_lats, return_dist=True)
+    dest_osmids, dropoff_dists = ox.distance.nearest_nodes(G, dest_lngs, dest_lats, return_dist=True)
+
+    filter = np.logical_and(np.array(start_dists) < 100, np.array(dropoff_dists) < 100)
+
+    flows_list = filter_list(flows_list, filter)
+    start_osmids = filter_list(start_osmids, filter)
+    dest_osmids = filter_list(dest_osmids, filter)
+
+    data_day = {}
+
+    for j in range(len(flows_list)):
+        
+        flow_number = flows_list[j]
+
+        if flow_number not in data_day:
+            data_day[flow_number] = defaultdict(int)
+
+        start_id = GET_NODEID[start_osmids[j]]
+        dest_id = GET_NODEID[dest_osmids[j]]
+        data_day[flow_number][(start_id, dest_id)] += 1
+    
+    return data_day 
 
 with open('data/green_tripdata_2016-236.csv', newline='') as csvfile:
     i = 0
     flow = 0
-    day = 3
+    day = 1
     amount = 0
     reader = csv.DictReader(csvfile)
     data_day = {flow: defaultdict(int)}
     start_time = time.time()
+    formatstring = '%Y-%m-%d %H:%M:%S'
+
+    # these lists respectively contain for each index i the flow number, the start latitude, the start longitude, the dest latitude and the dest longitude of one request
+    flows_list = []
+    start_lats = []
+    start_lngs = []
+    dest_lats = []
+    dest_lngs = []
+
     # Loop through rows csv
     for row in reader:
         new_time = datetime.datetime.strptime(row['lpep_pickup_datetime'], formatstring)
@@ -40,28 +80,38 @@ with open('data/green_tripdata_2016-236.csv', newline='') as csvfile:
         # Set begin time
         if i == 0:
             last_time = new_time
-        
+
         # Start new day file
         if new_time.day != last_time.day:
+
+            # Process the requets and write to txt file
+            data_day = process_requests(start_lngs, start_lats, dest_lngs, dest_lats, flows_list)
+            print_txt(data_day, day)
             day += 1
+            
+            # reset variables for next day
             flow = 0
             last_time = new_time
-            print_txt(data_day, day)
             data_day = {flow: defaultdict(int)}
-            
+            flows_list = []
+            start_lats = []
+            start_lngs = []
+            dest_lats = []
+            dest_lngs = []
+
         # Start new flow of 60 seconds
         if (new_time - last_time).total_seconds() > 59:
             last_time += datetime.timedelta(0,60)
             flow += 1
-            data_day[flow] = defaultdict(int)
-        
-        # Retrieve pickup and destination from csv 
-        pickup_osmid, dist_pickup = ox.distance.nearest_nodes(G, float(row['Pickup_longitude']), float(row['Pickup_latitude']), return_dist=True)
-        dest_osmid, dist_dropoff = ox.distance.nearest_nodes(G, float(row['Dropoff_longitude']), float(row['Dropoff_latitude']), return_dist=True)
 
-        if dist_pickup < 100 and dist_dropoff < 100:
-            data_day[flow][(osmid_to_nodeid[pickup_osmid], osmid_to_nodeid[dest_osmid])] += 1
+        flows_list.append(flow)
+        start_lats.append(float(row['Pickup_latitude']))
+        start_lngs.append(float(row['Pickup_longitude']))
+        dest_lats.append(float(row['Dropoff_latitude']))
+        dest_lngs.append(float(row['Dropoff_longitude']))
 
         i += 1
 
+    data_day = process_requests(start_lngs, start_lats, dest_lngs, dest_lats, flows_list)
     print_txt(data_day, day)
+
