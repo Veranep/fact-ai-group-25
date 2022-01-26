215fe3e83a25b27be56626bfd0342a1cdc0fa27a
Vera Neplenbroek <veraneplenbroek@icloud.com>
2022-01-26 11:22:50 +0100

    Change file path

--- preprocessing/flow_files_final.py
+++ preprocessing/flow_files_final.py
@@ -11,7 +11,7 @@ import time
 
 def print_txt(data, day):
     f = open(f"out/test_flow_5000_{day}.txt", 'w')
-    sys.stdout = f  
+    sys.stdout = f
     print(len(data))
     for key in data:
         print(f'Flows:{key}-{key}')
@@ -25,7 +25,7 @@ nodes = ox.graph_to_gdfs(G, edges=False)
 osmid_to_nodeid = dict(zip(list(nodes.index), list(range(nodes.shape[0]))))
 formatstring = '%Y-%m-%d %H:%M:%S'
 
-with open('~/fact-ai-rideshare/data/paper_replication/yellow_tripdata_2016-miniselection.csv', newline='') as csvfile:
+with open('../data/paper_replication/yellow_tripdata_2016-miniselection.csv', newline='') as csvfile:
     i = 0
     flow = 0
     day = 3
@@ -40,7 +40,7 @@ with open('~/fact-ai-rideshare/data/paper_replication/yellow_tripdata_2016-minis
         # Set begin time
         if i == 0:
             last_time = new_time
-        
+
         # Start new day file
         if new_time.day != last_time.day:
             day += 1
@@ -48,14 +48,14 @@ with open('~/fact-ai-rideshare/data/paper_replication/yellow_tripdata_2016-minis
             last_time = new_time
             print_txt(data_day, day)
             data_day = {flow: defaultdict(int)}
-            
+
         # Start new flow of 60 seconds
         if (new_time - last_time).total_seconds() > 59:
             last_time += datetime.timedelta(0,60)
             flow += 1
             data_day[flow] = defaultdict(int)
-        
-        # Retrieve pickup and destination from csv 
+
+        # Retrieve pickup and destination from csv
         pickup_osmid, dist_pickup = ox.distance.nearest_nodes(G, float(row['pickup_longitude']), float(row['pickup_latitude']), return_dist=True)
         dest_osmid, dist_dropoff = ox.distance.nearest_nodes(G, float(row['dropoff_longitude']), float(row['dropoff_latitude']), return_dist=True)
 
